8f102481f1d8ced3b7ec1496b3904f948d180150
Nima Karimipour <karimipour.nima@gmail.com>
2023-06-28 12:39:54 -0700

    improve script

--- analysis/annotator.py
+++ analysis/annotator.py
@@ -4,29 +4,36 @@ import shutil
 from pathlib import Path
 
 VERSION = '1.3.8-SNAPSHOT'
+BENCHMARK = 'opencms'
+OUT_DIR = '/tmp/ucr-tainting/{}'.format(BENCHMARK)
 ANNOTATOR_JAR = "{}/.m2/repository/edu/ucr/cs/riple/annotator/annotator-core/{}/annotator-core-{}.jar".format(str(Path.home()), VERSION, VERSION)
 REPO = subprocess.check_output(['git', 'rev-parse', '--show-toplevel']).strip().decode('utf-8')
 
 
 def prepare():
-    os.makedirs('/tmp/ucr-tainting/opencms', exist_ok=True)
-    shutil.rmtree('/tmp/ucr-tainting/opencms/0', ignore_errors=True)
-    with open('/tmp/ucr-tainting/opencms/paths.tsv', 'w') as o:
-        o.write("{}\t{}\n".format('/tmp/ucr-tainting/opencms/taint.xml', '/tmp/ucr-tainting/opencms/scanner.xml'))
+    os.makedirs(OUT_DIR, exist_ok=True)
+    shutil.rmtree('{}/0'.format(OUT_DIR), ignore_errors=True)
+    with open('{}/paths.tsv'.format(OUT_DIR), 'w') as o:
+        o.write("{}\t{}\n".format('{}/taint.xml'.format(OUT_DIR), '{}/scanner.xml'.format(OUT_DIR)))
 
 
 def run_annotator():
     prepare()
     commands = []
     commands += ["java", "-jar", ANNOTATOR_JAR]
-    commands += ['-d', '/tmp/ucr-tainting']
+    commands += ['-d', OUT_DIR]
     commands += ['-bc', 'cd {} && ./gradlew compileJava'.format(REPO)]
-    commands += ['-cp', '/tmp/ucr-tainting/opencms/paths.tsv']
+    commands += ['-cp', '{}/paths.tsv'.format(OUT_DIR)]
     commands += ['-i', 'edu.ucr.Initializer']
     commands += ['-n', 'edu.ucr.cs.riple.taint.ucrtainting.qual.RUntainted']
     commands += ['-cn', 'UCRTaint']
+    commands += ["--depth", "1"]
     # Uncomment to see build output
     # commands += ['-rboserr']
+    # Uncomment to disable outer loop
+    # commands += ['-dol']
+    # Uncomment to disable parallel processing
+    # commands += ['--disable-parallel-processing']
 
     subprocess.call(commands)
 
